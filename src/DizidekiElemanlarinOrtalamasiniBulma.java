44c7a7e448305196cc95b281457c7e1d2be5e653
Kerimaykutsengur <kaykut23@gmail.com>
2022-06-27 15:55:27 +0300

    Initial commit

---
+++ src/DizidekiElemanlarinOrtalamasiniBulma.java
@@ -0,0 +1,15 @@
+public class DizidekiElemanlarinOrtalamasiniBulma {
+    public static void main(String[] args) {
+        int[] list = {1, 2, 3, 4, 5};
+        double sum = 0.0;
+
+        for (int i = 0; i < list.length; i++) {
+            sum += list[i];
+        }
+        double average = sum / list.length;
+        System.out.print(average);
+
+
+    }
+}
+
